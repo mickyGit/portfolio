31230599f03ab0f77de289ba626e2597df6063d0
Mickael Leriche <leriche.mickael3@gmail.com>
2018-02-18 13:55:55 +0100

    ajout du formulaire de contact

--- inc/contact.php
+++ inc/contact.php
@@ -1,9 +1,19 @@
- <h1>Contact</h1>
-            <hr>
+<h1>Contact</h1>
+<hr>
 <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <image src="../images/maintenance.png"/>
-                    </div>
-                </div>
+    <div class="row">
+        <div class="col-md-6">
+            
+            <div>
+                <form method="post">
+                    <input type="text" name="nom"/>
+                    <input type="text" name="prenom"/>
+                    <textarea name="description"></textarea>
+                    <input type="text" name="nom"/>
+                    <input type="submit" value="envoyer"/>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-6"></div>
+    </div>
 </div>
