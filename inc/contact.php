9d0214acb9051598ab527405e369a87f2d92c67b
mickyGit <leriche.mickael3@gmail.com>
2018-02-18 14:16:40 +0100

    Update contact.php

--- inc/contact.php
+++ inc/contact.php
@@ -14,12 +14,9 @@
                 </form>
             </div>
         </div>
-<<<<<<< HEAD
+
         <div class="col-md-6">
             <p>ici sera mes infos</p>
         </div>
-=======
-        <div class="col-md-6"></div>
->>>>>>> 8e75bc5c9ee606ca4e313f75e6c9c1a4179f0db7
     </div>
 </div>
